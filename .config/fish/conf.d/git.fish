b2dfee261441703c0f171e7fe82fec3fc1c956a8
Kayce Basques <kaycebasques@gmail.com>
2026-04-08 16:08:25 -0700

    Clone Pigweed repo

--- .config/fish/conf.d/git.fish
+++ .config/fish/conf.d/git.fish
@@ -1,5 +1,9 @@
 #!/usr/bin/fish
 
+if not command -s git > /dev/null
+    sudo apt install git
+end
+
 if not test -e $HOME/.ssh/id_ed25519.pub
     echo "SSH key not found. Run `fish ~/cli/ssh.fish` to set up."
 end
@@ -12,3 +16,17 @@ git config --global core.editor "vim"
 git config --global user.name "Kayce Basques"
 git config --global user.email "kaycebasques@gmail.com"
 git config --global commit.gpgsign true
+
+if not test -d $HOME/pigweed
+    cd $HOME
+    git clone https://pigweed.googlesource.com/pigweed/pigweed
+    cd pigweed
+    set -l f (git rev-parse --git-dir)/hooks/commit-msg
+    mkdir -p (dirname $f)
+    curl -Lo $f https://gerrit-review.googlesource.com/tools/hooks/commit-msg
+    chmod +x $f
+    echo "TODO: go/pigweed-onboarding#git-config"
+    git config --local user.email "kayce@google.com"
+    git config --local commit.gpgsign false
+    cd -
+end	
