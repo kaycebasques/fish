78cf8b5f9552560a13271c2bce4f14120a26f1ac
Kayce Basques <kaycebasques@gmail.com>
2026-04-09 07:09:55 -0700

    Download Bazelisk

--- update.fish
+++ update.fish
@@ -7,6 +7,7 @@ cp $dir/.config/user-dir.dirs $HOME/.config/user-dir.dirs
 cp $dir/.config/fish/conf.d/* $HOME/.config/fish/conf.d/
 cp $dir/.config/fish/functions/* $HOME/.config/fish/functions/
 
+# TODO move this
 set default_dirs "Documents" "Downloads" "Music" "Pictures" "Public" "Templates" "Videos"
 for default_dir in $default_dirs
     set default_dir "$HOME/$default_dir"
@@ -14,4 +15,10 @@ for default_dir in $default_dirs
         continue
     end	
     rmdir $default_dir
+end
+
+# TODO move this
+if not test -f $HOME/bazelisk
+    curl -L https://github.com/bazelbuild/bazelisk/releases/latest/download/bazelisk-linux-amd64 -o $HOME/bazelisk
+    chmod +x $HOME/bazelisk
 end
