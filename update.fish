d83a7f1c3ed0591b011b3c84c212d232c2529f38
Kayce Basques <kaycebasques@gmail.com>
2026-04-10 12:29:58 -0700

    Exec fish at end of update.fish

--- update.fish
+++ update.fish
@@ -6,3 +6,5 @@ cp $dir/.bashrc $HOME/.bashrc
 cp $dir/.config/user-dir.dirs $HOME/.config/user-dir.dirs
 cp $dir/.config/fish/conf.d/* $HOME/.config/fish/conf.d/
 cp $dir/.config/fish/functions/* $HOME/.config/fish/functions/
+
+exec fish
